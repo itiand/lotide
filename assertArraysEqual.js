e4e590adb57f5792920dbd73108d480ce6eff06a
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-05-31 20:38:34 -0400

    find Key function implement

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,5 @@
 let eqArrays = function(arr1, arr2) {
-  if (!arr1 || !arr2) return false;
+  if (!arr1 || !arr2) return false; // Needs to be before testing arr1.length
   if (arr1.length !== arr2.length) return false;
 
   for (let i = 0; i < arr1.length; i++) {
