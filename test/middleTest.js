80e61f8d43662d695f389892a21a7349556abcb7
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-05 14:01:05 -0400

    implement m&c testing on tail and middle modules

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,29 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+describe("#middle", () => {
+  it("return an empty array if given array is just one item", () => {
+    assert.deepEqual(middle([1]), []);
+  }),
+
+    it("return an empty array if given an empty array", () => {
+      assert.deepEqual(middle([]), []);
+    });
+
+  it("reutrn [2] if given [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("return [3] if given [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("return two of middle item of a even-length array, eg [1, 2, 3, 4] --> [2,3]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
+
+
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const assert = require('chai').assert;
+const tail = require('../tail');
 
+describe("#tail", () => {
+  it("return everythin in the given array except for the first item. eg ['Yo Yo', 'Lighthouse', 'Labs'] --> ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]),['Lighthouse', 'Labs']);
+  })
+
+  it("should not change the original array", () => {
+    const originalArray = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(originalArray);
+    assert.deepEqual(originalArray, ["Yo Yo", "Lighthouse", "Labs"]);
+  });
+
+})
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ['Lighthouse', 'Labs'];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
