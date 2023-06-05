bc77ea261df488a0ed6e61cad67984bba8388b77
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-05 10:21:32 -0400

    refactor middle.js using module.export and require to test directory

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-let eqArrays = function(arr1, arr2) {
-  if (!arr1 || !arr2) return false;
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
 //modulo === 0 means even number -> two elements in the middle
 //means array[array.length / 2] and array[(array.length / 2) - 1] are the middle items
 //modulo !== 0 means odd number -> one element in the middle
@@ -38,9 +19,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
